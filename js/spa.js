1ace9c17f8293d1bb322a5d0049a78ed8cadcc6b
李海涛 <15393243613@163.com>
2017-09-20 08:26:27 +0800

    单独布局页面完成

---
+++ js/spa.js
@@ -0,0 +1,16 @@
+/**
+ * spa root modules
+ */ 
+var spa = (function(){
+    var initModule = function($container){
+        $container.html(
+            '<h1 style="display:inline-block;margin:25px;">'
+            +'hello world!'
+            +'</h1>'
+            
+        );
+    };
+    return {
+        initModule:initModule
+    }
+}());
