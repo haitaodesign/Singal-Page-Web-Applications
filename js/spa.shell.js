209ebc0a4757f565e955eef6ab8f0ca63b072aec
李海涛 <15393243613@163.com>
2017-09-21 09:04:11 +0800

    布局页css编写

---
+++ js/spa.shell.js
@@ -0,0 +1,23 @@
+/**
+ * spa.shell.js
+ * Shell modules for SPA
+ */ 
+
+ spa.shell=(function () {  
+    //------------------------ 变量声明 ------------------------
+    var configMap = {
+        main_html: String()+
+        '<div class="spa-shell-head">'+
+        '<div class="spa-shell-head-logo"></div>'+
+        ' <div class="spa-shell-head-acct"></div>'+
+        '<div class="spa-shell-head-search"></div>'+
+        '</div>'+
+        '<div class="spa-shell-main spa-x-closed">'+
+        '<div class="spa-shell-main-nav"></div>'+
+        '<div class="spa-shell-main-content"></div>'+
+        '</div>'+
+        '<div class="spa-shell-foot"></div>   '+
+        '<div class="spa-shell-chat"></div>'+
+        '<div class="spa-shell-modal"></div>'
+    }
+ }());
