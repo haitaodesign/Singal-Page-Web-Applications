657ca85cc63fccc81e32b0e6f4ad3b643bfd7fa2
李海涛 <15393243613@163.com>
2017-09-22 08:55:29 +0800

    动态生成布局页面

--- js/spa.shell.js
+++ js/spa.shell.js
@@ -18,6 +18,40 @@
         '</div>'+
         '<div class="spa-shell-foot"></div>   '+
         '<div class="spa-shell-chat"></div>'+
-        '<div class="spa-shell-modal"></div>'
+        '<div class="spa-shell-modal"></div>',
+        chat_extend_time:1000,
+        chat_retract_time:300,
+        chat_extend_height:450,
+        chat_retract_height:15
+    },
+    stateMap = { $container: null },
+    jqueryMap = {
+        // $container:$container,
+        // $chat:$container.find('.spa-shell-chat')
+    },
+    setJqueryMap,toggleChat,initModule;
+
+    // 缓存DOM方法
+    setJqueryMap = function(){
+        var $container = stateMap.$container,
+        jqueryMap = { $container: $container};
     }
+    // 事件处理函数
+    toggleChat = function(do_extend,callback){
+        var px_chat_ht = jqueryMap.$chat.height(),
+        is_open = px_chat_ht === configMap.chat_extend_height,
+        is_closed = px_chat_ht === configMap.chat_retract_height,
+        is_sliding = ! is_open && !is_closed;
+    }
+    //公共方法
+    initModule = function($container){
+        stateMap.$container = $container;
+        $container.html(configMap.main_html);
+        setJqueryMap();
+    }
+
+    return {
+        initModule: initModule
+    };
+
  }());
